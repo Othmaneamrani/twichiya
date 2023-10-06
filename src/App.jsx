92cff8676949dbb8f8bb444f7625c90642c269c0
othmaneamrani <othmaneamrani31@gmail.com>
2023-10-06 01:44:12 +0100

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Abir from '/Abir'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <Abir />
+    
+}
+
+export default App
