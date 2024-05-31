63160aabdeaa96302f28fbee32d6a2556354dc07
Othmaneamrani <othmaneamrani31@gmail.com>
2024-05-31 02:31:58 +0100

    context mferre9 x2

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,12 +3,14 @@ import { useContext, useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {PostContext} from "./PostContext.jsx";
+import {TestContext} from "./TestContext.jsx";
 
 
 export default function Posts () {
     const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
     const { setPostDet } = useContext(PostContext);
+    const {test , user} = useContext(TestContext);
 
 
     const handleGetPosts = async () => {
@@ -28,19 +30,21 @@ export default function Posts () {
       }, []);
     return(
         <div>
-        <h1>Posts</h1>
-        {posts && posts.length > 0 && (
-                        posts.map((post, index) => (
-                                <div key={index} >
-                                    <p>{post.id}</p>
-                                    <p>{post.title}</p>
-                                    <p>{post.content}</p>
-                                    <button onClick={() => handleDetails(post.id)} > Details</button>
-                                    <br />
-                                    <p>__________________________</p>
-                                </div>
-                            ))
-                    )}
-      </div>
+            <h1>Posts</h1>
+            <p>uu {user.nom}</p>
+            <p>tt {test.valeur}</p>
+            {posts && posts.length > 0 && (
+                posts.map((post, index) => (
+                    <div key={index}>
+                        <p>{post.id}</p>
+                        <p>{post.title}</p>
+                        <p>{post.content}</p>
+                        <button onClick={() => handleDetails(post.id)}> Details</button>
+                        <br/>
+                        <p>__________________________</p>
+                    </div>
+                ))
+            )}
+        </div>
     );
-  }
+}
